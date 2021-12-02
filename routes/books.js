e7854c6d1321d3242200e539c0df7e3eddf6fff7
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-12-02 05:22:34 -0600

    Create books.js
    Added books as a new route

---
+++ routes/books.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
+
+//Handler function to wrap each route
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+      } catch(error){
+        res.status(500).send(error);
+      }
+    }
+  }
+
+  router.get('/books', asyncHandler(async (req, res) => {
+    const books = await Book.findAll({ order: [['createdAt', 'DESC']]});
+    res.render('views/index', { books });
+  }));
