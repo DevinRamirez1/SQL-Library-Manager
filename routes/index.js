3c5f3708b4e15279337483fe791e1d4837db01c0
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-12-02 05:22:43 -0600

    Update index.js
    Moved code to books.js

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,9 @@ var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
 
-//Handler function to wrap each route
-function asyncHandler(cb){
-  return async(req, res, next) => {
-    try {
-      await cb(req, res, next)
-      } catch(error){
-        res.status(500).send(error);
-      }
-    }
-  }
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/books');
 });
 
-router.get('/books', asyncHandler(async (req, res) => {
-  const books = await Book.findAll({ order: [['createdAt', 'DESC']]});
-  res.render('views/index', { books });
-}));
 module.exports = router;
