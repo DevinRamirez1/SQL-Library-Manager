a3b2cff8b692374c878f93dce78110522989de54
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-12-07 17:48:44 -0600

    Update books.js
    Adding sequelize to require

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
+const Sequelize = require('sequelize');
+const { Op } = Sequelize.Op;
 
 //Handler function to wrap each route
 function asyncHandler(cb){
