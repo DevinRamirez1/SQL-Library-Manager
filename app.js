d37aadcd4d7c3e292acbb480bf8123beec376c2d
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-12-15 19:16:49 -0600

    Update app.js
    Moved error handlers to app.js

--- app.js
+++ app.js
@@ -25,19 +25,23 @@ app.use('/', indexRouter);
 app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  next(createError(404, 'That Page Does Not Exist'));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err.status, err.message);
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (res.status === 404) {
+  res.render('page-not-found', { err });
+  } else {
+    err.message = err.message || 'Uh-Oh. Looks something went wrong with the server.'
+    res.render('error', { err });
+  }
 });
 
 module.exports = app;
